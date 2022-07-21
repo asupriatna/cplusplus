f3fd5539ce8a911b823e3d2932734c7678cff27c
Agus Supriatna <agus.supriatna@gmail.com>
2022-07-21 09:35:47 +0700

    create main.cpp

---
+++ latihan_1/main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /*
+    File: main.cpp
+    Oleh: Develovers
+    Juli 2022
+    
+    Memahami struktur program C++
+    */
+
+    std::cout << "Halo\n";
+    std::cout << "Halo, Kita belajar pemrograman\n";
+    
+    // Mencetak baris dengan iterasi (pengulangan)
+    for (int i = 1; i <= 5; ++i) 
+    {
+            cout <<"Baris ke- " << i << "\n";
+    }
+    
+    x = y; y = y + 1; add(x, y);
+}
